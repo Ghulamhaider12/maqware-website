d843065f8e15d7a4069bb1cce818ea3379abe86f
aneesm96 <malik.anees96@gmail.com>
2023-06-05 17:45:41 +0500

    latest website code

---
+++ src/Utils/styles/color.js
@@ -0,0 +1,17 @@
+const white = '#FFF';
+const blue = '#55C5CE';
+const expertiseportfolioheading = '#BCBCBC';
+const expertiseclienttext = '#999999';
+const expertisebg = '#262626';
+const processtext = '#CCCCCC';
+
+export {
+    white,
+    blue,
+    expertiseportfolioheading,
+    expertiseclienttext,
+    expertisebg,
+    processtext
+}
+
+
---
+++ src/Utils/styles/styles.js
@@ -0,0 +1,32 @@
+import { expertiseclienttext, white } from "./color";
+
+export const styles = {
+    maintext: {
+        fontFamily: 'tex-grey-adventor-700',
+        fontSize: '86px',
+        color: white
+    },
+
+    maintext2: {
+        fontFamily: 'tex-grey-adventor-700',
+        fontSize: '46px',
+        color: white
+    },
+    cardtext_p: {
+        fontFamily: 'Roboto-300',
+        fontSize: '24px',
+        color: expertiseclienttext
+    },
+    
+    btntext: {
+        fontFamily: 'Roboto-400',
+        color: white,
+        fontSize: '24px'
+    },
+    btntext_form: {
+        fontFamily: 'Roboto-400',
+        color: white,
+        fontSize: '26px'
+    },
+
+}
